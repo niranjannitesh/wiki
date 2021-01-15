06f2e5b5b1f3085e040504f7ca8a3f0a86ff2c34
Nitesh Kumar Niranjan <hello@theux.dev>
2021-01-16 04:19:16 +0530

    feat: added cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from app.utils.boot import boot
 from app.models import db
 
 def create_app():
 
   app = Flask(__name__)
-
+  CORS(app)
 
   @app.before_request
   def before_request():
