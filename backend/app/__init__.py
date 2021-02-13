cafcace1faa2bd7f3c462bf1c80d43c14dab9a5d
Nitesh Kumar Niranjan <hello@theux.dev>
2021-02-13 23:17:06 +0530

    feat: re-boot on file changes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,24 +2,52 @@ from flask import Flask
 from flask_cors import CORS
 from app.utils.boot import boot
 from app.models import db
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+from config import DOCS_DIR, FILE_EXTENTIONS
+
 
 def create_app():
 
-  app = Flask(__name__)
-  CORS(app)
+    app = Flask(__name__)
+    CORS(app)
 
-  @app.before_request
-  def before_request():
-      db.connect()
+    @app.before_request
+    def before_request():
+        db.connect()
 
-  @app.after_request
-  def after_request(response):
-      db.close()
-      return response
+    @app.after_request
+    def after_request(response):
+        db.close()
+        return response
 
-  from app.api import api as api_blueprint
-  app.register_blueprint(api_blueprint, url_prefix='/api')
+    from app.api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
 
-  boot()
+    boot()
 
-  return app
+    def on_changed(event):
+        boot(connect=False)
+        print(f"something, {event.src_path}")
+
+    # watch files and restart
+    patterns = "*"
+    ignore_patterns = ""
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(
+        patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+    my_event_handler.on_created = on_changed
+    my_event_handler.on_deleted = on_changed
+    my_event_handler.on_modified = on_changed
+    my_event_handler.on_moved = on_changed
+
+    path = DOCS_DIR
+    go_recursively = True
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+    my_observer.start()
+
+    return app
--- backend/app/utils/boot.py
+++ backend/app/utils/boot.py
@@ -3,21 +3,24 @@ import re
 from config import DOCS_DIR, FILE_EXTENTIONS
 from app.models import db, File
 
-def boot():
-  db.connect()
-  db.drop_tables([File])
-  db.create_tables([File])
 
-  for path, subdirs, files in os.walk(DOCS_DIR):
-    for name in files:
-        filename, file_extension = os.path.splitext(name)
-        if file_extension in FILE_EXTENTIONS:
-          full_path = os.path.join(path, name)
-          relative_path = full_path.replace(DOCS_DIR, '.')
-          file_contents = open(full_path, "r").read()
-          title_regex = r"^#(?!#)(.*)"
-          match = re.search(title_regex, file_contents)
-          title = None
-          if match:
-            title = match.group(1)
-          file, created = File.get_or_create(full_path=full_path, relative_path=relative_path, title=title)
+def boot(connect=True):
+    if connect:
+        db.connect()
+    db.drop_tables([File])
+    db.create_tables([File])
+
+    for path, subdirs, files in os.walk(DOCS_DIR):
+        for name in files:
+            filename, file_extension = os.path.splitext(name)
+            if file_extension in FILE_EXTENTIONS:
+                full_path = os.path.join(path, name)
+                relative_path = full_path.replace(DOCS_DIR, '.')
+                file_contents = open(full_path, "r").read()
+                title_regex = r"^#(?!#)(.*)"
+                match = re.search(title_regex, file_contents)
+                title = None
+                if match:
+                    title = match.group(1)
+                file, created = File.get_or_create(
+                    full_path=full_path, relative_path=relative_path, title=title)
